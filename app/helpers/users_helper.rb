6b3d63fb30f8d1b542550e1db59b0cd50e79934d
greatscott <your.javelonagian@live.com>
2013-03-02 03:20:45 +0800

    kiido.ph FINAL as of March 2, 3:20am

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
 
   def gravatar_for(user, options = { :size=> 48 })    
-    default_url = "http://icons.iconarchive.com/icons/custom-icon-design/pretty-office-4/48/man-icon.png"
+    default_url = "http://i47.tinypic.com/2gx0j61.png"
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}" 
@@ -10,7 +10,7 @@ module UsersHelper
 
 
   def gravatar_edit(user, options = { :size=> 500 })
-    default_url = "http://icons.iconarchive.com/icons/custom-icon-design/pretty-office-4/256/man-icon.png"
+    default_url = "http://i47.tinypic.com/2gx0j61.png"
   	gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=500&d=#{CGI.escape(default_url)}"  
